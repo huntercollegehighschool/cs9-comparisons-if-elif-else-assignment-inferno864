6a966aaa124fc7374c4e53bfcb67fcdd548fff16
Colby O'Connor <94643942+inferno864@users.noreply.github.com>
2021-12-09 01:45:28 +0000

    Created the code

--- part4.py
+++ part4.py
@@ -24,3 +24,14 @@ opposite
 '''
 
 #start writing your code below
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+if (num1 > 0 and num2 > 0):
+  print("Positive")
+elif (num1 < 0 and num2 < 0):
+  print("Negative")
+elif (num1 == 0 or num2 == 0):
+  print("Zero")
+else:
+  print("Opposite")
